0b9180905ea81c6a6cfa7fe13092cdc73b1e71f3
samer <phillippes@gmail.com>
2023-04-06 14:47:17 +0200

    Added data for xp8 (graphs on 100 vertices) and xp9 (1msi/colour)

--- main.cpp
+++ main.cpp
@@ -22,7 +22,7 @@ bool CONVEX_RECOLORING_INSTANCE = false;   // e.g. as of ITOR'2022
 double RUN_CKS_WITH_TIME_LIMIT = 1800;
 
 bool WRITE_LATEX_TABLE_ROW = true;
-string LATEX_TABLE_FILE_PATH = string("xp7msi.dat");
+string LATEX_TABLE_FILE_PATH = string("xp9msi.dat");
 
 int main(int argc, char **argv)
 {
